54854e04f20678d7bf709741bbaa8cea47fe4362
Nafis Ahamed <nafisahamed14@gmail.com>
2024-03-03 23:57:56 +0600

    card almost done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Load = async(str = "")=>{
    const res = await fetch(url)
    const data = await res.json()
    console.log(data.posts);
+   console.log(data.posts[0].author.name);
     DisplayData(data.posts)
 }
 
@@ -21,15 +22,15 @@ setTimeout(()=>{
          div.innerHTML = `
          <div class="lg:flex bg-gray-200 ml-5 lg:ml-4 mt-2">
          <img src="${data.image}" class="w-[150px] h-[150px] lg:p-4" alt="">
-       <div class="lg:ml-3 lg:p-3 lg:mt-2">
-        <span class="mr-2">#</span><span class="font-extrabold">Music</span>
-        <span class="ml-2">Aurthor:</span><span>Awalad Hossain</span>
-        <p class="lg:text-3xl font-extrabold">World Cup Flags Football</p>
-        <p>you get different flag of different country <br> so buy it hurry up</p>
+       <div          class="lg:ml-3 lg:p-3 lg:mt-2">
+        <span class="mr-2">#</span><span class="font-extrabold">${data.category}</span>
+        <span class="ml-2">Aurthor:</span><span>${data.author.name}</span>
+        <p class="lg:text-3xl font-extrabold">${data.title}</p>
+        <p>${data.description}</p>
         <hr class="mt-2"><hr>
-        <span><i class="fa-regular fa-message"></i> <span class="ml-1">560</span> </span>
-        <span class="ml-1"><i class="fa-regular fa-eye"></i><span class="ml-1">1350</span></span>
-        <span class="ml-1"><i class="fa-regular fa-clock"></i><span class="ml-1">5</span></span>
+        <span><i class="fa-regular fa-message"></i> <span class="ml-1">${data.posted_time}</span> </span>
+        <span class="ml-1"><i class="fa-regular fa-eye"></i><span class="ml-1">${data.view_count}</span></span>
+        <span class="ml-1"><i class="fa-regular fa-clock"></i><span class="ml-1">${data.posted_time}</span></span>
         <span class="lg:ml-20 ml-8"><img src="../message.svg" class="rounded-3xl p-2 bg-green-400 inline" alt=""></span>
        </div>
        </div>
